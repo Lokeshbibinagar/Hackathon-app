dfab6ce09635f5f1a7be94db3f4df7650012783a
lokeshbibinagar <lokeshbibinagar97@gmail.com>
2021-01-04 21:57:11 +0530

    added firebase config
    Signed-off-by: lokeshbibinagar <lokeshbibinagar97@gmail.com>

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCyYNYguZ4S2NcBOg12acDvY4vHtzKxPxc",
+    authDomain: "hackathon-app-2e75d.firebaseapp.com",
+    projectId: "hackathon-app-2e75d",
+    storageBucket: "hackathon-app-2e75d.appspot.com",
+    messagingSenderId: "449652564282",
+    appId: "1:449652564282:web:a738b976ddc0f62cf43003",
+    measurementId: "G-BPFZHGWH45"
+  };
