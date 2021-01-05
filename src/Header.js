2ddf1b27f4f351ca7f2c6daf420b9fb175f2e16b
lokeshbibinagar <lokeshbibinagar97@gmail.com>
2021-01-06 00:29:17 +0530

    completed header
    Signed-off-by: lokeshbibinagar <lokeshbibinagar97@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import './Header.css'
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 
 function Header() {
     return (
         <div>
             <div className="header">
+
+           <div className="header_search">
+                <input className="search" type="text" placeHolder="Search"></input>
+                <SearchOutlinedIcon className="search_logo"></SearchOutlinedIcon>
+                <div className="header_right">
+                <div className="header_signIn">
+                    <span className="signIn">Sign In</span>
+                </div>
+                <div className="header_menu">
+                    <span className="menu">Menu</span>
+                </div>
+                <div className="header_profile">
+                    <span className="profile">My Profile</span>
+                </div>
+            </div>    
             </div>
+        
+            </div>
+            
+            
         </div>
     )
 }
