46f8e123115b53f1a165faaa8c58e85f5fabae91
lokeshbibinagar <lokeshbibinagar97@gmail.com>
2021-01-05 18:45:50 +0530

    body structure
    Signed-off-by: lokeshbibinagar <lokeshbibinagar97@gmail.com>

--- src/Middle.js
+++ src/Middle.js
@@ -5,7 +5,8 @@ function Middle() {
     return (
         <div className="middle">
             <img className="middle_image" src="https://images7.alphacoders.com/509/509786.jpg"></img>
-            
+            <h1 className="middle_name">CAFETERIA</h1>
+            <p className="middle_tagline">Lets Have A Cup Together</p>
         </div>
     )
 }
