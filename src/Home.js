a6d7542b9b191c2d61faef737a1197487148e03a
lokeshbibinagar <lokeshbibinagar97@gmail.com>
2021-01-08 14:48:52 +0530

    changes made
    Signed-off-by: lokeshbibinagar <lokeshbibinagar97@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Home.css'
 import Cards from './Cards'
-import BestCards from './BestCards'
 
 function Home() {
     return (
         <div className="home">
             <h1 className="todays-spl">Today's Special</h1>
-            <Cards />
+            <Cards classId='cards' heading='PISTACHIOOOOOOO' para='Embrace creamy coffee bliss with our new Pistachio Latte and Pistachio Frappuccino blended beverage.' image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67826.png' />
+            <Cards classId='cards_cyan' heading=' Portabella Mushroom Sous Vide Egg Bites' para='Power up with the Impossible Breakfast Sandwich or new Kale & Portabella Mushroom Sous Vide Egg Bites' image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67827.png'/>
             <h1 className="best-serve">Best Serve</h1>
-            <BestCards />
+            <Cards classId='cards_khaki' heading='vegetarian sandwich' para='Meatless vegetarian breakfast sandwich or wrap' image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67916.png' />
+            <Cards  classId='cards_choco' heading='NEW Honey Almondmilk Cold Brew' para='With a hint of honey and an almondmilk finish. ' image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67894.png'  />
+
         </div>
     )
 }
--- src/cards.js
+++ src/cards.js
@@ -1,18 +1,13 @@
  import React from 'react'
  import './Cards.css'
 
-function cards() {
+function cards({ classId, heading, para, image }) {
     return (
         <div className="cards_main">
-            <div className="cards">
-                <h2 className="card_heading">PISTACHIOOOOOOO</h2>
-                <p className="card_para">Embrace creamy coffee bliss with our new Pistachio Latte and Pistachio Frappuccino blended beverage.</p>
-                <img className="card_image" src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67826.png"></img>
-            </div>
-            <div className="card2">
-                <h2 className="card_heading">Mushroom Sous Vide Egg Bites.</h2>
-                <p className="card_para">Power up with the Impossible™ Breakfast Sandwich* or new Kale & Portabella Mushroom Sous Vide Egg Bites.</p>
-                <img className="card_image" src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67827.png"></img>
+            <div className={ classId } >
+                <h2 className="card_heading">{ heading }</h2>
+                <p className="card_para">{ para }</p>
+                <img className="card_image" src={ image }></img>
             </div>
             
         </div>
